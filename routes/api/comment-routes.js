30adde12b1cd4eae5fb73328ac5c6cda13f1087a
Aphillip22 <amber.rain.phillips@gmail.com>
2021-11-27 21:37:03 -0700

    add getters

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,4 +1,14 @@
 const router = require('express').Router();
 const { addComment, removeComment } = require('../../controllers/comment-controller');
 
+//add comment route to /api/comments/<pizzaId>
+router
+  .route('/:pizzaId')
+  .post(addComment);
+
+//remove comment route to /api/comments/<pizzaId>/<commentId>
+router
+  .route('/:pizzaId/:commentId')
+  .delete(removeComment);
+
 module.exports = router;
